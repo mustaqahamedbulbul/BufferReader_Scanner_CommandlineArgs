41a69bd753c591dc750605ba81c046e72c9d1b5b
mustaqahamedbulbul <mustaqbulbul1999@gmail.com>
2021-10-25 13:22:30 +0530

    User input in Java | BufferedReader | Scanner | Command line args.

---
+++ src/com/unacademy/Main.java
@@ -0,0 +1,20 @@
+package com.unacademy;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String argument = args[0];
+        System.out.println("Argument received is " + argument);
+
+       // Scanner sc = new Scanner(System.in);
+
+      //  String s = sc.nextLine();
+      //  System.out.println("String input is " + s);
+
+     //   int i = sc.nextInt();
+     //   System.out.println("int input is " + i);
+    }
+}
